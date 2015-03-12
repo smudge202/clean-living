6fc077a332d0cd1a0472e5162e733a800d26c0bc
Smudge202 <pete_201@hotmail.com>
2015-03-12 21:00:37 +0000

    StateEngine introduced

---
+++ src/Engine/StateEngine.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CleanLiving.Engine
+{
+    public class StateEngine
+    {
+        public StateEngine(object x) { throw new ArgumentNullException(); }
+    }
+}
---
+++ tests/Engine.Tests/StateEngineTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CleanLiving.Engine.Tests
+{
+    public class StateEngineTests
+    {
+        [Fact]
+        public void WhenClockNotProvidedThenThrowsException()
+        {
+            Action act = () => new StateEngine(null);
+            act.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
