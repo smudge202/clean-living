d25d5baf8232fc667a5c845c71006d4a01a089a4
Smudge202 <pete_201@hotmail.com>
2015-03-12 21:13:09 +0000

    Missing GameTime from last commit

---
+++ src/Engine/GameTime.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanLiving.Engine
+{
+    public class GameTime
+    {
+        public static GameTime Now { get; } = new GameTime();
+
+        public object Add(int x) { return new object(); }
+    }
+}
