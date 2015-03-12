8088f16f81e5a6ae0ec47d1904e2c3a6d5691399
Smudge202 <pete_201@hotmail.com>
2015-03-12 20:33:40 +0000

    Test Attributes brought over from (soon to be deleted) actor-health branch Namespace/Assembly name correction to test project

--- tests/GameEngine.Tests/Properties/AssemblyInfo.cs
+++ tests/GameEngine.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GameEngine.Tests")]
+[assembly: AssemblyTitle("CleanLiving.GameEngine.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GameEngine.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("CleanLiving.GameEngine.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
