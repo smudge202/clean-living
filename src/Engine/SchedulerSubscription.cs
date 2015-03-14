bd84c0325d3df07309d46b5c302266bb965ae393
Smudge202 <pete_201@hotmail.com>
2015-03-14 02:08:24 +0000

    Started on thread mechanisms for scheduler but need fresh eyes to continue.

---
+++ src/Engine/SchedulerSubscription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CleanLiving.Engine
+{
+    internal class SchedulerSubscription : IDisposable
+    {
+        private readonly IObserver<long> _observer;
+
+        public SchedulerSubscription(IObserver<long> observer)
+        {
+            _observer = observer;
+        }
+
+        public void Dispose() { }
+    }
+}
