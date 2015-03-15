a04014ef098c5c6f7386bd8ffa998e8249e17bd2
Smudge202 <pete_201@hotmail.com>
2015-03-15 17:12:37 +0000

    Introduced Hydration Resolved some reference issues

---
+++ src/Models/Hydration.cs
@@ -0,0 +1,30 @@
+﻿using CleanLiving.Engine;
+using System;
+
+namespace CleanLiving.Models
+{
+    public class Hydration : IDisposable, IObserver<GameTime>
+    {
+        public Hydration(IEngine engine)
+        {
+            engine.Subscribe(this, GameTime.Now);
+        }
+
+        public void OnNext(GameTime value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose() { }
+    }
+}
---
+++ tests/Models.Tests/HydrationTests.cs
@@ -0,0 +1,20 @@
+﻿using CleanLiving.Engine;
+using CleanLiving.Models;
+using CleanLiving.TestHelpers;
+using Moq;
+
+namespace Models.Tests
+{
+    public class HydrationTests
+    {
+        [UnitTest]
+        public void WhenHydrationCreatedThenSchedulesDiminishTime()
+        {
+            var engine = new Mock<IEngine>();
+            using (var hydration = new Hydration(engine.Object))
+            {
+                engine.Verify(m => m.Subscribe(hydration, It.IsAny<GameTime>()), Times.Once);
+            }
+        }
+    }
+}
