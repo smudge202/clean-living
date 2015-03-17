b2e9a6d88db6ef0554cc60568e11d206b986975f
Smudge202 <pete_201@hotmail.com>
2015-03-17 19:21:06 +0000

    Small (non-functional) change to kick off CI

--- src/Game/Internal/Hydration.cs
+++ src/Game/Internal/Hydration.cs
@@ -6,7 +6,10 @@ namespace CleanLiving.Game.Internal
 {
     public class Hydration : Models.Hydration<DateTime, TimeSpan>
     {
-        public Hydration(IOptions<TimeOptions<DateTime, TimeSpan>> config, ITimeFactory<DateTime, TimeSpan> factory, IGameEngine engine) 
+        public Hydration(
+            IOptions<TimeOptions<DateTime, TimeSpan>> config, 
+            ITimeFactory<DateTime, TimeSpan> factory, 
+            IGameEngine engine) 
             : base(config, factory, engine) { }
     }
 }
