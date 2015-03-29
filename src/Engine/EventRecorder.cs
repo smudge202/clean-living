f974a7f204105f8b2601b2bc69817b35ab8b05b8
Smudge202 <pete_201@hotmail.com>
2015-03-29 15:53:00 +0100

    Missing Event Recorder from previous commit

---
+++ src/Engine/EventRecorder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CleanLiving.Engine
+{
+    internal sealed class EventRecorder : IObserver<IMessage>
+    {
+        private readonly IEngine _engine;
+        private readonly ISerializeEvents _serialize;
+        private readonly IPersistEvents _persist;
+
+        public EventRecorder(IEngine engine, ISerializeEvents serialize, IPersistEvents persist)
+        {
+            if (engine == null) throw new ArgumentNullException(nameof(engine));
+            if (serialize == null) throw new ArgumentNullException(nameof(serialize));
+            if (persist == null) throw new ArgumentNullException(nameof(persist));
+            _engine = engine;
+            _serialize = serialize;
+            _persist = persist;
+
+            _engine.Subscribe(this);
+        }
+
+        public void OnNext(IMessage value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
